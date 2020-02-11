fc5519c7159aa43077b3e5bf94d91fc6c8d01969
Prokop Simek <prokop@dxheroes.io>
2020-02-11 20:29:44 +0100

    feat: add Config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: undefined,
+      database: 'dashboard_data_collector',
+      entities: [],
+      synchronize: true,
+    }),
+    ConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/config.interface.ts
@@ -0,0 +1,4 @@
+export enum ConfigKeys {
+  GOOGLE_AUTH = 'GOOGLE_AUTH',
+  GOOGLE_SHEET_ID = 'GOOGLE_SHEET_ID',
+}
